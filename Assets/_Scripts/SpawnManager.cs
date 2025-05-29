20b2529654783ca454b38afda6d864eff53aadba
Amanda Reyes <amandabreyesr@gmail.com>
2025-05-29 16:50:48 -0600

    Added spawn obstacles manager

---
+++ Assets/_Scripts/SpawnManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    private float startDelay = 1;
+    private float repeatDeay = 2;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnObstacle), startDelay, repeatDeay);
+    }
+
+    void SpawnObstacle()
+    {
+        int prefabIndex = Random.Range(0, obstaclePrefabs.Length);
+        Instantiate(obstaclePrefabs[prefabIndex], transform.position, obstaclePrefabs[prefabIndex].transform.rotation);
+    }
+}
