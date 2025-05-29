37619a951bace8df5d159ef861fe1a7133e46743
Amanda Reyes <amandabreyesr@gmail.com>
2025-05-29 15:08:35 -0600

    first commit

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float jumpForce = 10f;
+    public float gravityMultiplier = 2;
+    public bool isOnGround = true;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); //F = m * a
+        }
+    }
+}
