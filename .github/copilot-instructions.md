eaf0d2d7d96afb596ab0d8f9eb4d1de01ee9ba0f
Amanda Reyes <amandabreyesr@gmail.com>
2025-09-22 00:47:45 -0600

    Added and finished Challenge 3

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: 22-Jump
 - Unity version: Unity 6000.0.47f1
 - Active game object:
-  - Name: Destroy Obstacles
-  - Tag: Untagged
+  - Name: Player
+  - Tag: Player
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+    private bool isHighEnough;
+
+    private float yBound = 14;
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip bounceSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isHighEnough = transform.position.y >= yBound;
+
+        if (isHighEnough)
+        {
+            transform.position = new Vector3(transform.position.x, yBound, transform.position.z);
+            playerRb.AddForce(Vector3.down * 1.2f, ForceMode.Impulse);
+        }
+
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver && !isHighEnough)
+        {
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound, 1.5f);
+        }
+
+        // if player collides with bomb, explode and set gameOver to true
+        else if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+            Invoke(nameof(RestartGame), 1.5f);
+        }
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
